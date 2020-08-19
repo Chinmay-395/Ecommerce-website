36e1c30747bf3f2622087ccd4da50f0e84b42099
Chinmay-395 <chinmaydali395@gmail.com>
2020-08-20 00:04:10 +0530

    New endeavour

--- ecommerce/store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import cart, checkout, store, updateItem
-
-urlpatterns = [
-    path('', store, name="store"),
-    path('cart/', cart, name="cart"),
-    path('checkout/', checkout, name="checkout"),
-    path('update_item/', updateItem, name="update_item")
-]
